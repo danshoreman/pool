fa1a9c6295ec859a89e6f7622306a93d4bbe4f7a
Dan Shoreman <dan@danshoreman.co.uk>
2015-11-23 22:27:15 +0000

    Adding grunt

--- www/Gruntfile.js
+++ www/Gruntfile.js
@@ -5,27 +5,45 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		sass: {
+			options: {
+				sourceMap: true,
+				outputStyle: 'compressed'
+			},
 			dist: {
-				options: {
-					style: 'expanded'
-				},
 				files: {
-					'library/css/style.css': 'library/scss/style.scss'
+					'library/css/style.css': 'library/scss/style.scss',
+					'library/css/critical.css': 'library/scss/critical.scss',
+					'library/css/ie.css': 'library/scss/ie.scss'
 				}
 			}
-		},
+		},		
 		watch: {
 			css: {
 				files: '**/*.scss',
-				tasks: ['sass']
+				tasks: ['sass'],
+				options: {
+		      livereload: 35729
+		    }
 			}
+		},
+		imageoptim: {
+		  myTask: {
+		    options: {
+		      jpegMini: true,
+		      imageAlpha: true,
+		      quitAfter: true
+		    },
+		    src: ['library/images']
+		  }
 		}
 	});
-
 	// Load the Grunt plugins.
-	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-imageoptim');
 	
 	// Register the default tasks.
-	grunt.registerTask('dev', ['watch']);
+	grunt.registerTask('dev', ['sass:dist', 'watch']);
+	grunt.registerTask('imageoptim', ['imageoptim']);
+
 };
