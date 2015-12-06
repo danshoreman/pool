74ed4f619b4c3b48d7154287013fd1f1593b9319
Dan Shoreman <dan@danshoreman.co.uk>
2015-12-06 21:44:41 +0000

    Adding transition & positioning central box

--- www/library/js/scripts.js
+++ www/library/js/scripts.js
@@ -4,8 +4,12 @@
 		
 		$(document).on('click','.begin-btn', function(e) {
 			e.preventDefault();
+			// welcome page
 			$('.main-logo').addClass('logo-click');
 			$('.begin-btn').addClass('begin-none');
+			
+			// add player screen
+			$('.add-players').addClass('player-position');
 		});
 	});
 })(jQuery);
